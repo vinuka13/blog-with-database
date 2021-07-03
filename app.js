34d19957b7c69537e9741f2c46f163a4e1f681a7
unknown <pussepitiyavinuka@gmail.com>
2021-07-03 23:11:15 +0530

    add gitignore

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-vinuka:vinuka13@cluster0.cz3xp.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 var posts = []
 
@@ -84,6 +84,11 @@ app.get("/posts/:postID", function(req, res){
 
 });
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
